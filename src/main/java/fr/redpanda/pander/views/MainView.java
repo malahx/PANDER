230aa0c888b1fd3b811fdc90e3a027e4c8656831
malahx <gwenolelh@aol.com>
2017-08-22 12:01:09 +0200

    added footer

--- src/main/java/fr/redpanda/pander/views/MainView.java
+++ src/main/java/fr/redpanda/pander/views/MainView.java
@@ -13,6 +13,7 @@ import java.awt.Insets;
 public class MainView extends JFrame {
 
 	private JPanel contentPane;
+	private JPanel pnlFooter;
 
 	/**
 	 * Launch the application.
@@ -40,16 +41,16 @@ public class MainView extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		GridBagLayout gbl_contentPane = new GridBagLayout();
-		gbl_contentPane.columnWidths = new int[]{0, 0, 0};
-		gbl_contentPane.rowHeights = new int[]{0, 0, 0};
-		gbl_contentPane.columnWeights = new double[]{1.0, 1.0, Double.MIN_VALUE};
-		gbl_contentPane.rowWeights = new double[]{1.0, 1.0, Double.MIN_VALUE};
+		gbl_contentPane.columnWidths = new int[] {0, 0};
+		gbl_contentPane.rowHeights = new int[] {0, 0, 0};
+		gbl_contentPane.columnWeights = new double[]{0.0, 1.0};
+		gbl_contentPane.rowWeights = new double[]{0.0, 1.0, 0.0};
 		contentPane.setLayout(gbl_contentPane);
 		
 		JPanel pnlNavbar = new JPanel();
 		GridBagConstraints gbc_pnlNavbar = new GridBagConstraints();
 		gbc_pnlNavbar.gridwidth = 2;
-		gbc_pnlNavbar.insets = new Insets(0, 0, 5, 5);
+		gbc_pnlNavbar.insets = new Insets(0, 0, 5, 0);
 		gbc_pnlNavbar.fill = GridBagConstraints.BOTH;
 		gbc_pnlNavbar.gridx = 0;
 		gbc_pnlNavbar.gridy = 0;
@@ -57,7 +58,7 @@ public class MainView extends JFrame {
 		
 		JPanel pnlSidebar = new JPanel();
 		GridBagConstraints gbc_pnlSidebar = new GridBagConstraints();
-		gbc_pnlSidebar.insets = new Insets(0, 0, 0, 5);
+		gbc_pnlSidebar.insets = new Insets(0, 0, 5, 5);
 		gbc_pnlSidebar.fill = GridBagConstraints.BOTH;
 		gbc_pnlSidebar.gridx = 0;
 		gbc_pnlSidebar.gridy = 1;
@@ -65,10 +66,20 @@ public class MainView extends JFrame {
 		
 		JPanel pnlContent = new JPanel();
 		GridBagConstraints gbc_pnlContent = new GridBagConstraints();
+		gbc_pnlContent.insets = new Insets(0, 0, 5, 0);
 		gbc_pnlContent.fill = GridBagConstraints.BOTH;
 		gbc_pnlContent.gridx = 1;
 		gbc_pnlContent.gridy = 1;
 		contentPane.add(pnlContent, gbc_pnlContent);
+		
+		pnlFooter = new JPanel();
+		GridBagConstraints gbc_pnlFooter = new GridBagConstraints();
+		gbc_pnlFooter.gridwidth = 2;
+		gbc_pnlFooter.insets = new Insets(0, 0, 0, 5);
+		gbc_pnlFooter.fill = GridBagConstraints.BOTH;
+		gbc_pnlFooter.gridx = 0;
+		gbc_pnlFooter.gridy = 2;
+		contentPane.add(pnlFooter, gbc_pnlFooter);
 	}
 
 }
