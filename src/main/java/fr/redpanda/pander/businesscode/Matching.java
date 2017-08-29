0c934f57a2bd30d6fbb4b97e852ead382264e4d6
Patrice SCHOCH <patriceschoch@yahoo.fr>
2017-08-29 18:13:24 +0200

    Business code

--- src/main/java/fr/redpanda/pander/businesscode/Matching.java
+++ src/main/java/fr/redpanda/pander/businesscode/Matching.java
@@ -1,12 +1,14 @@
 package fr.redpanda.pander.businesscode;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import fr.redpanda.pander.entities.Candidate;
 import fr.redpanda.pander.entities.Company;
 import fr.redpanda.pander.entities.Job;
 import fr.redpanda.pander.entities.Skill;
+import fr.redpanda.pander.entities.TypeSkill;
 
 public class Matching {
 
@@ -14,113 +16,80 @@ public class Matching {
 	private Company company;
 	private Job job;
 	private int calculateRatio;
-	
+
+	/**
+	 * @param candidate
+	 * @param company
+	 * @param job
+	 */
+	public Matching(Candidate candidate, Company company, Job job) {
+		super();
+		this.candidate = candidate;
+		this.company = company;
+		this.job = job;
+	}
 
 	private List<Skill> getTechSkills(List<Skill> skills) {
-		return null;
+		List<Skill> returnTechSkills = new ArrayList<>();
+		for (int i = 0; i < skills.size(); i++) {
+			Skill skill = skills.get(i);
+			if (skill.getType().equals(TypeSkill.TECH)) {
+				returnTechSkills.add(skill);
+			}
+		}
+		return returnTechSkills;
 	}
-	
+
 	private List<Skill> getSoftSkills(List<Skill> skills) {
-		return null;
+		List<Skill> returnSoftSkills = new ArrayList<>();
+		for (int i = 0; i < skills.size(); i++) {
+			Skill skill = skills.get(i);
+			if (skill.getType().equals(TypeSkill.SOFT)) {
+				returnSoftSkills.add(skill);
+			}
+		}
+		return returnSoftSkills;
 	}
-	
-	private List<Skill> getEqualSkills(List<Skill> candidateSkills, Skill jobSkills) {
-		return null;
-	}
-	
-	public int matchingCalc() 
-		matchingResult = 
-		return ;
+		
+
+
+	private List<Skill> getEqualSkills(List<Skill> candidateSkills, List<Skill> jobSkills) {
+		List<Skill> returnEqualSkills = new ArrayList<>();
+		for (int i = 0; i < candidateSkills.size(); i++) {
+			Skill candidateSkill = candidateSkills.get(i);
+			
+			for (int j = 0; j < jobSkills.size(); j++) {
+				Skill jobSkill = jobSkills.get(j);
+			
+				if (candidateSkill.equals(jobSkill)) {
+				returnEqualSkills.add(jobSkill);
+				}
+			}
+		}
+		return returnEqualSkills;
 	}
 
-	// Variables pour fixer des boucles éventuelles.
-	int ii = 0;
-	int jj = 0;
 	
-	// Variable pour le résultat du matching.
-	int matchingResult;
+	
+	public double matchingCalc() {
 
-	// Recherche de la taille des listes afin de pouvoir calculer les ratios de calcul du matching.
-	int SkillsNumber = Skill.size();
-	int SkillsNumberCandidate = candidateSkills.size();
-	int SkillsNumberJob = jobSkills.size();
-	
-	
+		List<Skill> jobSkills = job.getSkills();
+		List<Skill> candidateSkills = candidate.getSkills();
+		List<Skill> jobTechSkills = getTechSkills(jobSkills);
+		List<Skill> candidateTechSkills = getTechSkills(candidateSkills);
+		List<Skill> jobSoftSkills = getSoftSkills(jobSkills);
+		List<Skill> candidateSoftSkills = getSoftSkills(candidateSkills);
+
+		List<Skill> equalTechSkills = getEqualSkills(candidateTechSkills, jobTechSkills);
+		List<Skill> equalSoftSkills = getEqualSkills(candidateSoftSkills, jobSoftSkills);
+
+		double techMatchingResult = (equalTechSkills.size()) / (jobTechSkills.size());
+		double softMatchingResult = (equalSoftSkills.size()) / (jobSoftSkills.size());
+		double jokerSkills = (jobTechSkills.size() - equalTechSkills.size()) / (jobTechSkills.size());
+
+		double matchingResult = (techMatchingResult * 60) + (softMatchingResult * 30) + (jokerSkills * 10);
+
+		return matchingResult;
+	}
 
 }
-
-
-
-//
-//
-////ALGOROTHME PANDER CONNECTION
-////
-////- L'algorithme doit prendre des données issues des compétences d'un candidat
-////mais également les compétences demandées pour un poste proposé.
-////- Il doit comparer les compétences identiques entre le candidat et le poste.
-////- Il doit proposer un taux de matching correspondant au pourcentage des
-////compétences nécessaires.
-//
-//
-//ALGORITHME MATCHING_PANDER_CONNECTION
-//
-//CONSTANTE
-//	
-//	ENTIER : CS_T <- 10
-//	ENTIER : JS_T <- 10
-//
-//VARIABLE
-//
-//	// PRISE DES TABLES DE DONNEES CORRESPONDANT AUX COMPETENCES DES CANDIDATS
-//	// ET AUX POSTES PROPOSES
-//
-//	TABLEAU<CHAINE>[CS_T] : CandidateSkills
-//	TABLEAU<CHAINE>[JS_T] : JobSkills
-//
-//
-//	// COMPTEURS :
-//
-//	ENTIER : ii <- 0
-//	ENTIER : jj <- 0
-//
-//
-//	// MATCH POINT :
-//
-//	REEL : mp <- 0
-//
-//
-//	// TABLEAU DES COMPETENCES COMMUNES :
-//
-//	TABLEAU<CHAINE>[1] : CommonSkills
-//
-//
-//	// VARIABLE DE CALCUL :
-//
-//	ENTIER : n <- 0
-//
-//
-//DEBUT
-//
-//	// 1. CALCUL DE CORRESPONDANCE ENTRE LES TABLEAUX :
-//
-//	POUR ii DE 0 A CS_T -1 PAS DE 1 FAIRE
-//		POUR jj DE 0 A JS_T -1 PAS DE 1 FAIRE
-//
-//			SI CandidateSkills[ii] = JobSkills[jj] ALORS
-//				n <- Len(CommonSkills)
-//				CommonSkills[n-1] <- CandidateSkills[ii]
-//				Redim(CommonSkills, n +1)
-//
-//			FINSI
-//
-//		FINPOUR
-//	FINPOUR 
-//
-//
-//	// 2. CALCUL DU RESULTAT DE MATCHING :
-//
-//	mp <- Len(CommonSkills) / Len(JobSkills)
-//
-//	Ecrire("Matching de : " & (mp*100) & " %")  
-//
-//FIN
