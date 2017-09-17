6227938fb6b75e15a0a09b1c1f4c8edd5ea40baf
Malah <malah@neuf.fr>
2017-09-17 17:28:22 +0200

    clean

--- src/main/java/fr/redpanda/pander/controllers/MatchingCtrl.java
+++ src/main/java/fr/redpanda/pander/controllers/MatchingCtrl.java
@@ -7,7 +7,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.List;
 
-import javax.swing.JFrame;
 import javax.swing.JTable;
 import javax.swing.ListSelectionModel;
 import javax.swing.event.ListSelectionEvent;
